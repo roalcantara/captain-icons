6c448bf8af11b76c2479cbb03fa91aa056b800d8
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-06-10 18:50:53 -0300

    build(tasks): Add clean task
    Which cleans up the dist folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 'use strict';
 
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 
-gulp.task('hello:world', function () {
-  console.log('Hello world!')
+gulp.task('dist:clean', function () {
+  return gulp.src('./dist', {read: false})
+    .pipe(clean());
 });
+
+gulp.task('build', [
+  'dist:clean'
+]);
